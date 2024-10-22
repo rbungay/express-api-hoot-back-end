27cd23df78af532758d89b867be2ad5da6d4ed8b
Rani Bungay <ranibungay@gmail.com>
2024-10-22 15:57:04 -0400

    first commit on dev

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const testJWTRouter = require('./controllers/test-jwt');
-const usersRouter = require('./controllers/users');
-const profilesRouter = require('./controllers/profiles');
+const mongoose = require("mongoose");
+const testJWTRouter = require("./controllers/test-jwt");
+const usersRouter = require("./controllers/users");
+const profilesRouter = require("./controllers/profiles");
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 app.use(cors());
 app.use(express.json());
 
 // Routes go here
-app.use('/test-jwt', testJWTRouter);
-app.use('/users', usersRouter);
-app.use('/profiles', profilesRouter);
+app.use("/test-jwt", testJWTRouter);
+app.use("/users", usersRouter);
+app.use("/profiles", profilesRouter);
 
 app.listen(3000, () => {
-    console.log('The express app is ready!');
+  console.log("The express app is ready!");
 });
+
+console.log("push this now");
