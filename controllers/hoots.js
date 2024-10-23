e84e3bd800a54264fc3935e4af4b37aa618744ce
Rani Bungay <ranibungay@gmail.com>
2024-10-22 20:30:12 -0400

    create index hoots router

---
+++ controllers/hoots.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const hoots = await Hoot.find({})
+      .populate("author")
+      .sort({ createdAt: "desc" });
+    res.status(200).json(hoots);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
