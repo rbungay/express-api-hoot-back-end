e592b16a354d98c359df5b9369d915eb2d666695
RosarioManny <rosario.emm47@gmail.com>
2024-10-22 23:26:55 -0400

    Add Comment Route

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -34,6 +34,25 @@ router.put("/:hootId", async (req, res) => {
   }
 });
 
+router.post('/:hootId/comments', async (req, res) => {
+  try {
+    req.body.author = req.user._id;
+    const hoot = await Hoot.findById(req.params.hootId);
+    hoot.comments.push(req.body);
+    await hoot.save();
+
+    // Find the newly created comment:
+    const newComment = hoot.comments[hoot.comments.length - 1];
+
+    newComment._doc.author = req.user;
+
+    // Respond with the newComment:
+    res.status(201).json(newComment);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 router.delete("/:hootId", async (req, res) => {
   try {
     const hoot = await Hoot.findById(req.params.hootId);
