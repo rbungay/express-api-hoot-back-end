bee68b14a197d318f669c4450f1615e569bc7265
paolasoria <paola.soria0597@gmail.com>
2024-10-23 01:21:58 -0400

    add create route and show route

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -1,5 +1,31 @@
 const express = require("express");
+const verifyToken=require('../middleware/verify-token.js');
+const Hoot= require('../models/hoot.js');
 const router = express.Router();
+
+
+router.use(verifyToken);
+
+router.post('/',async(req,res)=> {
+  console.log("Creating Data")
+  try {
+    req.body.author= req.user._id;
+    const hoot= await Hoot.create(req.body);
+    hoot._doc.author=req.user;
+    res.status(201).json(hoot);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+router.get('/:hootId', async(req,res)=>{
+   try {
+     const hoot=await Hoot.findById(req.params.hootId).populate('author');
+     res.status(200).json(hoot);
+   } catch (error) {
+     res.status(500).json(error);
+   }
+});
 
 router.get("/", async (req, res) => {
   try {
@@ -48,3 +74,5 @@ router.delete("/:hootId", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+module.exports = router;
