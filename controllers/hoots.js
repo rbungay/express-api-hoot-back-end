81e9b8e939a78e6020bbfd40295b1a9b47f5bd57
Rani Bungay <ranibungay@gmail.com>
2024-10-22 20:49:50 -0400

    create delete router

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -33,3 +33,18 @@ router.put("/:hootId", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+router.delete("/:hootId", async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+
+    if (!hoot.author.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+    const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+    res.status(200).json(deletedHoot);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
